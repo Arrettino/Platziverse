a586ae0cac5a6be6280d2ec09b1f4a37deb4a262
Arrettino <batukearrettino@gmail.com>
2020-06-09 18:54:45 -0300

    ./agent add class platziverseAgent and connect/disconnect functions

---
+++ agent/index.js
@@ -0,0 +1,31 @@
+const EventEmmitters = require("events");
+
+class PlatziverseAgent extends EventEmmitters {
+  constructor(opts) {
+    super();
+    this._options = opts;
+    this._timer = null;
+    this._started = false;
+  }
+
+  connect() {
+    if (!this._started) {
+      this._started = true;
+      this.emit("connected");
+      const opts = this._options;
+      this._timer = setInterval(() => {
+        this.emit("agent/message", "this is a message");
+      }, opts.interval);
+    }
+  }
+
+  disconnect() {
+    if (this._started) {
+      clearInterval(this._timer);
+      this._started = false;
+      this.emit("disconnected");
+    }
+  }
+}
+
+module.exports = PlatziverseAgent;
